90d932a762a7b5c5550e731b5d620dcb86af68e2
lucascost <lcyancosta@gmailc.om>
2022-11-06 17:47:49 -0300

    adicionado: Perfil e Anuncio

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Perfil, Anuncio
+
+admin.site.register(Perfil)
+admin.site.register(Anuncio)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class Perfil(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=15)
+    estado = models.CharField(max_length=2)
+    cidade = models.CharField(max_length=32)
+    bairro = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.nome
+
+class Anuncio(models.Model):
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    tipo_animal = models.CharField(max_length=15)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
